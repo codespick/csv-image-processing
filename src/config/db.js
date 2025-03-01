618434e6ab164ba3980234a3b004ac8b90dfed83
gzone2434 <gzone2434@gmail.com>
2025-03-01 14:26:24 +0530

    fix the issue

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-require("dotenv").config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.ATLAS_URL);
-    mongoose.set('strictQuery', false);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.ATLAS_URL, { family: 4 });
+    mongoose.set("strictQuery", false);
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
